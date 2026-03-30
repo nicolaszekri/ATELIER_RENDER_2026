b6776894543828dc7deb06b21281d17444cfe05f
Nicolas Zekri <106680973+nicolaszekri@users.noreply.github.com>
2026-03-30 16:00:49 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,32 +2,30 @@ terraform {
   required_providers {
     render = {
       source  = "render-oss/render"
-      version = "1.8.0"
+      version = ">= 1.7.0"
     }
   }
 }
 
-variable "render_api_key" {}
-variable "owner_id" {}
-variable "github_actor" {}
-
 provider "render" {
-  api_key = var.render_api_key
+  api_key  = var.render_api_key
+  owner_id = var.render_owner_id
+}
+
+variable "github_actor" {
+  description = "GitHub username"
+  type        = string
 }
 
 resource "render_web_service" "flask_app" {
-  name          = "flask-app"
-  plan          = "free"
-  region        = "frankfurt"
-  start_command = "python app.py"
+  name   = "flask-render-iac-${var.github_actor}"
+  plan   = "free"
+  region = "frankfurt"
 
   runtime_source = {
-    native_runtime = {
-      auto_deploy   = true
-      branch        = "main"
-      build_command = "pip install -r requirements.txt"
-      repo_url      = "https://github.com/nicolaszekri/ATELIER_RENDER_2026"
-      runtime       = "python"
+    image = {
+      image_url = var.image_url
+      tag       = var.image_tag
     }
   }
 
