2e8ea889f7def37e4c8c36234f584292a11f862b
Nicolas Zekri <106680973+nicolaszekri@users.noreply.github.com>
2026-03-30 17:41:49 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,54 +1,61 @@
+# --- CONFIGURATION DU PROVIDER ---
 terraform {
   required_providers {
     render = {
       source  = "render-oss/render"
-      version = "1.8.0"
+      version = ">= 1.7.0"
     }
   }
 }
 
 provider "render" {
-  api_key = var.render_api_key
+  api_key  = var.render_api_key
+  owner_id = var.render_owner_id
 }
 
+# --- 1. BASE DE DONNÉES POSTGRESQL ---
+resource "render_postgres" "postgres_db" {
+  name           = "db-postgre-${var.github_actor}"
+  plan           = "free"
+  region         = "frankfurt"
+  version        = "15" 
+  database_name  = "workshop_db"
+  database_user  = "user_admin"
+}
+
+# --- 2. BACKEND FLASK (WEB SERVICE) ---
 resource "render_web_service" "flask_app" {
-  name          = "flask-app"
-  plan          = "free"
-  region        = "frankfurt"
-  start_command = "python app.py"
-
-  runtime_source = {
-    native_runtime = {
-      auto_deploy   = true
-      branch        = "main"
-      build_command = "pip install -r requirements.txt"
-      repo_url      = "https://github.com/nicolaszekri/ATELIER_RENDER_2026"
-      runtime       = "python"
-    }
-  }
-
-  env_vars = {
-    ENV = {
-      value = "production"
-    }
-  }
-}
-
-resource "render_web_service" "adminer" {
-  name          = "adminer"
-  plan          = "free"
-  region        = "frankfurt"
-  start_command = "docker run -p 10000:8080 adminer"
+  name   = "flask-render-iac-${var.github_actor}"
+  plan   = "free"
+  region = "frankfurt"
 
   runtime_source = {
     image = {
-      image_url = "docker.io/adminer:latest"
+      image_url = var.image_url
+      tag       = var.image_tag
     }
   }
 
   env_vars = {
-    ADMINER_DEFAULT_SERVER = {
-      value = "YOUR_POSTGRES_HOST"
+    ENV = { 
+      value = "production" 
+    }
+    DATABASE_URL = { 
+      value = render_postgres.postgres_db.connection_info.internal_connection_string 
+    }
+  }
+}
+
+# --- 3. ADMINER (OUTIL DE GESTION DB) ---
+resource "render_web_service" "adminer" {
+  name   = "adminer-${var.github_actor}"
+  plan   = "free"
+  region = "frankfurt"
+
+  runtime_source = {
+    image = {
+      image_url = "adminer" 
+      tag       = "latest"
     }
   }
 }
