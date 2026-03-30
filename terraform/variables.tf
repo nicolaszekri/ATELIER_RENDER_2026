209d030f0aeb3e650841bebf0a85bdae675b404e
Nicolas Zekri <106680973+nicolaszekri@users.noreply.github.com>
2026-03-30 17:43:51 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,21 @@
+variable "github_actor" {
+  description = "Le nom d'utilisateur GitHub fourni par les GitHub Actions"
+  type        = string
+}
+
 variable "render_api_key" {
   type      = string
   sensitive = true
 }
 
-variable "owner_id" {
+variable "render_owner_id" {
   type = string
 }
 
-variable "github_actor" {
+variable "image_url" {
+  type = string
+}
+
+variable "image_tag" {
   type = string
 }
