ba34d3732231bb47b32c2375e2a047cb94836ee0
Nicolas Zekri <106680973+nicolaszekri@users.noreply.github.com>
2026-03-30 16:06:42 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,36 +2,53 @@ terraform {
   required_providers {
     render = {
       source  = "render-oss/render"
-      version = ">= 1.7.0"
+      version = "1.8.0"
     }
   }
 }
 
 provider "render" {
-  api_key  = var.render_api_key
-  owner_id = var.render_owner_id
-}
-
-variable "github_actor" {
-  description = "GitHub username"
-  type        = string
+  api_key = var.render_api_key
 }
 
 resource "render_web_service" "flask_app" {
-  name   = "flask-render-iac-${var.github_actor}"
-  plan   = "free"
-  region = "frankfurt"
+  name          = "flask-app"
+  plan          = "free"
+  region        = "frankfurt"
+  start_command = "python app.py"
 
   runtime_source = {
-    image = {
-      image_url = var.image_url
-      tag       = var.image_tag
+    native_runtime = {
+      auto_deploy   = true
+      branch        = "main"
+      build_command = "pip install -r requirements.txt"
+      repo_url      = "https://github.com/nicolaszekri/ATELIER_RENDER_2026"
+      runtime       = "python"
     }
   }
 
   env_vars = {
     ENV = {
       value = "production"
+    }
+  }
+}
+
+resource "render_web_service" "adminer" {
+  name          = "adminer"
+  plan          = "free"
+  region        = "frankfurt"
+  start_command = "docker run -p 10000:8080 adminer"
+
+  runtime_source = {
+    image = {
+      image_url = "docker.io/adminer:latest"
+    }
+  }
+
+  env_vars = {
+    ADMINER_DEFAULT_SERVER = {
+      value = "YOUR_POSTGRES_HOST"
     }
   }
 }
